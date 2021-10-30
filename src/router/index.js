3956973913c0a01e847e71a7cdbf2b5cff5f2879
Héctor Arturo Noyola Mondragón <totenoyola@hotmail.com>
2021-10-30 09:55:53 -0500

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,11 @@ const routes = [
     component: Casos
   },
   {
+    path: '/Home',
+    name: 'Home',
+    component: Home
+  },
+  {
     path: '/pokemon/:pokemon',
     name: 'Pokemon',
     component: Pokemon
